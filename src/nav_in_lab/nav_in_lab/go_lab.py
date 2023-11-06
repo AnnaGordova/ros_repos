fb9baa21600931809e53422a0860d28b9352b296
Ann <annagordova2@gmail.com>
2023-11-06 21:11:06 +0300

    Robot has turned to rhe right side

--- src/nav_in_lab/nav_in_lab/go_lab.py
+++ src/nav_in_lab/nav_in_lab/go_lab.py
@@ -2,22 +2,30 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 class LaserAngleAndDistance: #класс угол-расстояние
     
-    def __init__(self, angle, dist):
+    def __init__(self, angle, range):
         self.angle = angle
-        self.dist = dist
+        self.range = range
 
     def toStr(self):
-        s = "Angle: " + str(self.angle) + " Distance: " + str(self.dist)
+        s = "Angle: " + str(self.angle) + " Distance: " + str(self.range)
         return s
     
+    def get_angle(self):
+        return self.angle
+    
+    def gen_range(self):
+        return self.range
+    
 class LaserScanSubscriberNode(Node):
-
     def __init__(self):
         super().__init__("Laser_subscriber")
         self.laser_subscriber_ = self.create_subscription(LaserScan, "/scan", self.laser_callback, 10)
+        self.cmd_vel_pub_ = self.create_publisher(Twist, "/cmd_vel", 10)
+
 
     def laser_callback(self, msg: LaserScan):
         self.angle_min = msg.angle_min #задаем поля для углов
@@ -41,7 +49,22 @@ class LaserScanSubscriberNode(Node):
             self.get_logger().info(self.AngRangeList[i].toStr())'''
         
         
-        self.get_logger().info(self.AngRangeList[5].toStr()) #вывод для проверки
+        #self.get_logger().info(self.AngRangeList[5].toStr()) #вывод для проверки
+
+        #------------------------------------------------- закончили обработку входных данных
+        minlenind = ranges.index(min(ranges))
+        msg = Twist()
+        
+        d = abs(self.AngRangeList[minlenind].get_angle() - self.AngRangeList[0].get_angle()) 
+        if not(0.0 <= d < 0.5):
+            msg.angular.z = 0.35
+        else:
+            msg.angular.z = 0.0
+        self.get_logger().info(str(self.AngRangeList[minlenind].get_angle()) + " " + str(self.AngRangeList[0].get_angle()))
+        self.cmd_vel_pub_.publish(msg)    
+            
+        r
+        
         
 
         
